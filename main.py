4cff41e1a6c58ea57dbc7561dbcc47b35a1255d9
posydomoika <omfgwow76@gmail.com>
2023-04-04 23:59:47 +0300

    new paint

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from os import listdir
 import pygame_menu
 
 
@@ -13,11 +15,12 @@ class Interface:
         и записывает все подходящие файлы в переменую self.models : [str]
         :return: None
         """
-        pass
+        self.models = [i for i in listdir() if "Model" in i]
 
     def __init__(self, size_window=448, model="Model_CNN_ex_128_10", fl_ci=True):
-        pg.init()
+        self.sc = pg.display.set_mode((size_window, size_window))
         self.search()
+        print(self.models)
         """
         :param size_window: размер окна
         :param model: название модели(путь к ней)
@@ -28,12 +31,18 @@ class Interface:
         self.fl_ci = fl_ci
         self.model = keras.models.load_model(model)
         self.size_window = size_window
-        self.sc = pg.display.set_mode((size_window, size_window))
         pg.display.set_caption("Neural Network Interface")
-        # self.menu =
+        self.pool = keras.models.load_model("Pool")
+
+        # self.menu = pygame_menu.Menu('Welcome', 400, 300,
+        #                         theme=pygame_menu.themes.THEME_BLUE)
+
+        # self.menu.add.text_input('Name :', default='John Doe')
+        # self.menu.add.selector('Difficulty :', [(k,n) for n,k in enumerate(self.models)], onchange=lambda *ar:print(1))
+        # self.menu.add.button('Play', self)
+        # self.menu.add.button('Quit', pygame_menu.events.EXIT)
 
     def __call__(self):
-        # self.menu.mainloop(self.sc)
         clock = pg.time.Clock()
         fl_draw = False
         sw = self.size_window
@@ -54,34 +63,38 @@ class Interface:
                     case pg.KEYDOWN:
                         if event.key == pg.K_TAB:
                             x3 = pg.surfarray.pixels3d(self.sc)
-                            result = []
-                            temp = sw // 28
-                            for i in range(0, sw, temp):
-                                for j in range(0, sw, temp):
-                                    summ = 0
+                            x3 = np.array(x3)[:, :, 0].T.copy() / 255
+                            x3.shape = (1, 448, 448, 1)
 
-                                    for i1 in range(i, i + temp):
-                                        for j2 in range(j, j + temp):
-                                            summ += x3[i1][j2][0] / 255
-                                    summ = x3[i][j][0] / 255
-                                    result.append(summ)
+                            result = np.array(self.pool(x3))
+                            copy_res = result.squeeze()
+                            plt.imshow(result.squeeze(), cmap=plt.cm.binary)
+                            plt.show()
+                            for i in range(28):
+                                for j in range(28):
+                                    col = result[0][i][j][0] * 255
+                                    rect = pg.draw.rect(self.sc,
+                                                        (col, col, col),
+                                                        (j * 16, i * 16,
+                                                         j * 16 + 16, i * 16 + 16))
+                                    pg.display.update(rect)
 
-                            result = np.ndarray((28, 28), buffer=np.array(result[::-1]),
-                                                dtype=float)
-                            result = np.rot90(result, k=1, axes=(0, 1))
-                            result = np.fliplr(result)
-                            result = tf.reshape(tf.cast(result, tf.float32), [1, 28, 28, 1])
                             res = self.model(result)
-
-                            for i in range(len(res[0])):
-                                if max(res[0]) == res[0][i]:
-                                    print(f"Result: {i}\nConfidence:{res.numpy()[0][i]}")
-                                    break
-
+                            n = np.argmax(res)
+                            print(f"Result: {n}\nConfidence:{res[0][n]}")
                         elif event.key == pg.K_CAPSLOCK:
                             update_sc = pg.draw.circle(self.sc, (0, 0, 0), (sw // 2, sw // 2), sw)
                             pg.display.update(update_sc)
-
+                        elif event.key == pg.K_0:
+                            print(*[f"{i + 1}) {k}" for i, k in enumerate(self.models)],
+                                  sep="\n")
+                            while True:
+                                try:
+                                    n = int(input("Введите номер модели")) - 1
+                                    self.model = keras.models.load_model(self.models[n])
+                                    break
+                                except:
+                                    print("Ты оладушек")
             if fl_draw and self.fl_ci:
                 circle1 = pg.draw.circle(self.sc, (255, 255, 255), (x, y), sw * 0.036)
                 pg.display.update(circle1)
@@ -96,5 +109,5 @@ class Interface:
 
 
 if __name__ == '__main__':
-    inter = Interface()
+    inter = Interface(fl_ci=True, model="Model_ex_64_32")
     inter()
