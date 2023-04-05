6bc3a45a51a035ea2ec14b33e0848abfb7e4abb1
posydomoika <omfgwow76@gmail.com>
2023-04-05 14:34:04 +0300

    ADD menu and  opt menu

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pygame as pg
 import numpy as np
 from tensorflow import keras
-import tensorflow as tf
-import matplotlib.pyplot as plt
 from os import listdir
-import pygame_menu
 import sys
+from time import time
 
 
 class Interface:
@@ -21,9 +19,8 @@ class Interface:
     def __init__(self, model="Model_CNN_ex_128_10"):
         """
         :param model: название модели(путь к ней)
-        :param fl_ci: True - это рисовать кругами,False - это рисовать квадратами
-        (это не будет влиять на результат)
         """
+
         self.sc = pg.display.set_mode((448 + 60, 448))
         pg.display.set_caption("Neural Network Interface")
         self.search()
@@ -35,16 +32,16 @@ class Interface:
 
     def __call__(self):
         pg.init()
-        print(pg.font.get_fonts())
         clock = pg.time.Clock()
-        pg.display.update()
         fl_draw = False
         fl_menu = False
         sw = self.size_window
         x, y = 0, 0
 
-        self.menu()
+        f = pg.font.SysFont('arial', 50)
+        text_menu = f.render("M", True, (255, 255, 255))
 
+        self.update(sw, text_menu)
 
         while True:
             for event in pg.event.get():
@@ -58,34 +55,41 @@ class Interface:
                         fl_draw = True
                     case pg.MOUSEBUTTONUP:
                         fl_draw = False
-                    case pg.KEYDOWN:
+                    case pg.KEYDOWN if fl_menu is False:
                         if event.key == pg.K_TAB:
                             x3 = pg.surfarray.pixels3d(self.sc)
-                            x3 = np.array(x3)[:, :, 0].T [:448,:448] / 255
+                            print(len(x3[0]))
+                            x3 = np.array(x3)[0:448, :, 0].T / 255
 
-                            # plt.imshow(x3.squeeze(), cmap=plt.cm.binary)
-                            # plt.show()
                             x3.shape = (1, 448, 448, 1)
 
                             result = np.array(self.pool(x3))
                             copy_res = result.squeeze()
-                            # plt.imshow(result.squeeze(), cmap=plt.cm.binary)
-                            # plt.show()
-                            for i in range(28):
-                                for j in range(28):
-                                    col = result[0][i][j][0] * 255
-                                    rect = pg.draw.rect(self.sc,
-                                                        (col, col, col),
-                                                        (j * 16, i * 16,
-                                                         j * 16 + 16, i * 16 + 16),0)
-                                    pg.display.update(rect)
+
+                            draw_see_model = []
 
                             res = self.model(result)
                             n = np.argmax(res)
                             print(f"Result: {n}\nConfidence:{res[0][n]}")
+
+                            for j in range(28):
+                                for i in range(28):
+                                    col = result[0][i][j][0] * 255
+                                    rect = pg.draw.rect(self.sc,
+                                                        (col, col, col),
+                                                        (j * 16, i * 16,
+                                                         j * 16 + 16, i * 16 + 16))
+                                    draw_see_model.append(rect)
+
+                            self.update(sw, text_menu, draw_see_model)
+
+                            res = self.model(result)
+                            n = np.argmax(res)
+                            print(f"Result: {n}\nConfidence:{res[0][n]}")
+
                         elif event.key == pg.K_CAPSLOCK:
                             update_sc = pg.draw.rect(self.sc, (0, 0, 0),
-                                                     (0, 0, self.size_window, self.size_window),
+                                                     (0, 0, sw, sw),
                                                      sw)
                             pg.display.update(update_sc)
                         elif event.key == pg.K_0:
@@ -101,44 +105,38 @@ class Interface:
                                 except:
                                     print("Ты оладушек ")
 
-            if fl_draw and x < self.size_window-15:
+            if fl_draw and x < sw - 15:
                 circle1 = pg.draw.circle(self.sc, (255, 255, 255), (x, y), sw * 0.036)
                 pg.display.update(circle1)
 
             elif fl_draw and 455 <= x <= 510 and 0 <= y <= 40:
-                col_menu = (255,255,255) if fl_menu is False else (0,0,0)
+                col_menu = (255, 255, 255) if fl_menu is False else (0, 0, 0)
 
                 update_menu = pg.draw.rect(self.sc, col_menu,
-                                            (0, 0, self.size_window, self.size_window),
-                                            sw)
+                                           (0, 0, sw, sw),
+                                           sw)
                 pg.display.update(update_menu)
                 fl_menu = not fl_menu
                 fl_draw = False
 
-
-
         clock.tick(120)
 
-    def menu(self):
-        f = pg.font.SysFont('arial', 50)
+    def update(self, sw, text=None, draw_see_model=None):
 
         menu = pg.draw.rect(self.sc,
                             (100, 100, 100),
-                            (self.size_window, 0, self.size_window + 60, self.size_window),
-                            self.size_window)
-        pg.display.update(menu)
+                            (sw, 0, sw + 60, sw),
+                            sw)
 
-        res_model = [
-            # f.render((f"{i}-{res}",), True, (255, 255, 255))
+        self.sc.blit(text, (sw + 9, -10))
 
-        ]
-
-        text = f.render("M", True, (255, 255, 255))
-        self.sc.blit(text, (self.size_window + 9, -10))
-
-        pg.display.update()
+        # if draw_see_model:
+        #     [pg.display.update(rect) for rect in draw_see_model]
+        # else:
         pg.display.update()
 
+    def draw_result(self):
+        """Отрисовывает результат нейронной сети"""
 
 
 if __name__ == '__main__':
