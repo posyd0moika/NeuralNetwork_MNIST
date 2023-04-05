3653b3376e46adc4076901de6405b0ceacfc03f4
posydomoika <omfgwow76@gmail.com>
2023-04-05 21:25:37 +0300

    add borders for models in menu

--- main.py
+++ main.py
@@ -15,19 +15,22 @@ class Interface:
         :return: None
         """
         self.models = [i for i in listdir() if "Model" in i]
+        for i in range(2):
+            self.models.append(f"Model_{i}")
 
-    def __init__(self, model="Model_CNN_ex_128_10"):
+    def __init__(self, model_name="Model_CNN_ex_128_10"):
         """
-        :param model: название модели(путь к ней)
+        :param model_name: название модели(путь к ней)
         """
 
+        self.model_name = model_name
         self.sc = pg.display.set_mode((448 + 60, 448))
         pg.display.set_caption("Neural Network Interface")
         self.search()
 
         self.size_window = 448
 
-        self.model = keras.models.load_model(model)
+        self.model = keras.models.load_model(model_name)
         self.pool = keras.models.load_model("Pool")
 
     def __call__(self):
@@ -37,9 +40,32 @@ class Interface:
         fl_menu = False
         sw = self.size_window
         x, y = 0, 0
-
         f = pg.font.SysFont('arial', 50)
+        f2 = pg.font.SysFont('arial', 30)
         text_menu = f.render("M", True, (255, 255, 255))
+        step_model = sw // len(self.models)
+        act_model = (
+            f2.render(self.model_name, True, (50, 50, 50)),
+            self.model_name,
+            (0, 0))
+
+        text_models = [
+            (
+                f2.render(mod, True, (100, 100, 100)),
+                mod,
+                (448 // 6, step_model * num)
+            )
+            for num, mod in enumerate(self.models, start=1)
+        ]
+
+        self.borders = [
+            (
+                (448 // 6, step_model * num),
+                (448 // 6 + len(mod) * 18, step_model * num + 38)
+            )
+            for num, mod in enumerate(self.models, start=1)
+        ]
+
         self.update(sw, text_menu)
 
         while True:
@@ -50,88 +76,116 @@ class Interface:
                         sys.exit()
                     case pg.MOUSEMOTION:
                         x, y = event.pos
+                        print(x, y)
                     case pg.MOUSEBUTTONDOWN:
                         fl_draw = True
                     case pg.MOUSEBUTTONUP:
                         fl_draw = False
-                    case pg.KEYDOWN if fl_menu is False:
-                        if event.key == pg.K_TAB:
-                            x3 = pg.surfarray.pixels3d(self.sc)
-                            print(len(x3[0]))
-                            x3 = np.array(x3)[0:448, :, 0].T / 255
+                    case pg.KEYDOWN if event.key == pg.K_TAB and fl_menu is False:
+                        x3 = pg.surfarray.pixels3d(self.sc)
+                        print(len(x3[0]))
+                        x3 = np.array(x3)[0:448, :, 0].T / 255
 
-                            x3.shape = (1, 448, 448, 1)
+                        x3.shape = (1, 448, 448, 1)
 
-                            result = np.array(self.pool(x3))
-                            copy_res = result.squeeze()
+                        result = np.array(self.pool(x3))
+                        copy_res = result.squeeze()
 
-                            draw_see_model = []
+                        # draw_see_model = []
 
-                            res = self.model(result)
-                            n = np.argmax(res)
-                            print(f"Result: {n}\nConfidence:{res[0][n]}")
+                        res = self.model(result)
+                        n = np.argmax(res)
+                        print(f"Result: {n}\nConfidence:{res[0][n]}")
 
-                            for j in range(28):
-                                for i in range(28):
-                                    col = result[0][i][j][0] * 255
-                                    rect = pg.draw.rect(self.sc,
-                                                        (col, col, col),
-                                                        (j * 16, i * 16,
-                                                         j * 16 + 16, i * 16 + 16))
-                                    draw_see_model.append(rect)
+                        for j in range(28):
+                            for i in range(28):
+                                col = result[0][i][j][0] * 255
+                                rect = pg.draw.rect(self.sc,
+                                                    (col, col, col),
+                                                    (j * 16, i * 16,
+                                                     j * 16 + 16, i * 16 + 16))
+                                # draw_see_model.append(rect)
 
-                            self.update(sw, text_menu, draw_see_model)
+                        self.update(sw, text_menu)
 
-                            res = self.model(result)
-                            n = np.argmax(res)
-                            print(f"Result: {n}\nConfidence:{res[0][n]}")
+                    case pg.KEYDOWN if event.key == pg.K_CAPSLOCK and fl_menu is False:
+                        update_sc = pg.draw.rect(self.sc, (0, 0, 0),
+                                                 (0, 0, sw, sw),
+                                                 sw)
+                        pg.display.update(update_sc)
 
-                        elif event.key == pg.K_CAPSLOCK:
-                            update_sc = pg.draw.rect(self.sc, (0, 0, 0),
-                                                     (0, 0, sw, sw),
-                                                     sw)
-                            pg.display.update(update_sc)
-                        elif event.key == pg.K_0:
-                            print(
-                                *[f"{i + 1}) {k}" for i, k in enumerate(self.models)],
-                                sep="\n"
-                            )
-                            while True:
-                                try:
-                                    n = int(input("Введите номер модели")) - 1
-                                    self.model = keras.models.load_model(self.models[n])
-                                    break
-                                except:
-                                    print("Ты оладушек ")
+                    case pg.KEYDOWN if event.key == pg.K_0 and fl_menu is False:
+                        print(
+                            *[f"{i + 1}) {k}" for i, k in enumerate(self.models)],
+                            sep="\n"
+                        )
+                        while True:
+                            try:
+                                n = int(input("Введите номер модели")) - 1
+                                self.model = keras.models.load_model(self.models[n])
+                                break
+                            except:
+                                print("Ты оладушек ")
 
-            if fl_draw and x < sw - 15:
+            if fl_draw and x < sw - 15 and not fl_menu:
                 circle1 = pg.draw.circle(self.sc, (255, 255, 255), (x, y), sw * 0.036)
                 pg.display.update(circle1)
 
             elif fl_draw and 455 <= x <= 510 and 0 <= y <= 40:
                 col_menu = (255, 255, 255) if fl_menu is False else (0, 0, 0)
 
-                update_menu = pg.draw.rect(self.sc, col_menu,
-                                           (0, 0, sw, sw),
-                                           sw)
-                pg.display.update(update_menu)
+                pg.draw.rect(self.sc, col_menu,
+                             (0, 0, sw, sw),
+                             sw)
+                if fl_menu is False:
+                    self.update(sw, menu=False, text_models=text_models, activate_model=act_model)
+
+                pg.display.update()
                 fl_menu = not fl_menu
                 fl_draw = False
 
+            if fl_menu is True:
+                pass
         clock.tick(120)
 
-    def update(self, sw, text=None, draw_see_model=None):
+    def update(self, sw, text=None, text_models=None, menu=True, activate_model=None):
+        """Обновляет главное меню(создает его)
+        так же отвечает за прорисовку как видит рисунок модель"""
 
-        menu = pg.draw.rect(self.sc,
-                            (100, 100, 100),
-                            (sw, 0, sw + 60, sw),
-                            sw)
+        if menu:
+            pg.draw.rect(self.sc,
+                         (100, 100, 100),
+                         (sw, 0, sw + 60, sw),
+                         sw)
+        if text:
+            self.sc.blit(text, (sw + 9, -10))
 
-        self.sc.blit(text, (sw + 9, -10))
+        if text_models:
+            """
+            x,y = x,y 
+            x2,y2 = x + len()* 18,y + 38"""
+            for mod, name, (x, y) in text_models:
+                pg.draw.rect(self.sc,
+                             (255, 0, 0),
+                             (x, y, x + len(name) * 14, 38)
+                             )
+                print(x, y, x + 14 * len(name), y + 38)
+            for mod, name, (x, y) in text_models:
+                self.sc.blit(mod, (x, y))
 
-        # if draw_see_model:
-        #     [pg.display.update(rect) for rect in draw_see_model]
-        # else:
+        if activate_model:
+            """
+            x1,y1 = 0,0
+            x2,y2 = len() * 18, 38
+            """
+            mod, name, (x, y) = activate_model
+            pg.draw.rect(self.sc,
+                         (255, 0, 0),
+                         (x, y, x + len(name) * 15, 38)
+                         )
+
+            self.sc.blit(mod, (x, y))
+
         pg.display.update()
 
     def draw_result(self):
@@ -139,5 +193,5 @@ class Interface:
 
 
 if __name__ == '__main__':
-    inter = Interface(model="Model_ex_64_32")
+    inter = Interface(model_name="Model_ex_64_32")
     inter()
